b096e88677f1dc926f91007d0da9593cbd889416
Michel Lavezzo <michellavezzo@gmail.com>
2023-06-06 00:28:36 -0300

    feat: split_data function done (choose randomly validation or training), Minor fix at read_images_folder

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from PIL import Image
 
 root_path = os.getcwd()
@@ -41,11 +42,11 @@ def read_images_in_folders(directory, folder_list):
     list = []
     for folder in folder_list:
         subfolder = os.path.join(directory, folder)
+        iris_list = []
         for filename in os.listdir(subfolder):
             file_path = os.path.join(subfolder, filename)
             if os.path.isfile(file_path) and os.path.splitext(filename)[1].lower() in ('.png'):
                 with open(file_path, 'rb') as image_file:
-                    iris_list = []
                     value = image_file.read()
                     item = Item(path=file_path, folder_name=folder, bin_value=value, type='idle')
                     iris_list.append(item)
@@ -64,12 +65,47 @@ def load_data():
     grayscale_folders = get_folder_list(directory=grayscale_path)
     return read_images_in_folders(directory=grayscale_path, folder_list=grayscale_folders)
 
+# CHOOSE ONE RANDOM IMAGE FROM EACH FOLDER TO BE VALIDATION DATA AND THE REST TO BE TRAINING DATA
+def split_data(data):
+    # [ 
+    #     [ { folder_name, path, type, bin_value }, { folder_name, path, type, bin_value }, ... ] , 
+    #     [ { ... }, { ... }, ... ] 
+    # ]
+    training_data = []
+    validation_data = []
+    n_files_in_folder = len(data[0])
+    for idx, iris in enumerate(data):
+        iris_training_data = []
+        iris_validation_data = []
+        random_number = random.randint(0, (n_files_in_folder - 1))
+        # DEBUG
+        # print('idx', idx)
+        for index, item in enumerate(iris):
+            if index == random_number or (index == (len(iris) - 1) and len(iris_validation_data) == 0):
+                item.type = 'validation'
+                iris_validation_data.append(item)
+                # DEBUG 
+                # print('validation', item.path, ' ' ,index) 
+            else:
+                item.type = 'training'
+                iris_training_data.append(item)
+                # DEBUG 
+                # print('training', item.path, ' ' ,index)
+
+        training_data.append(iris_training_data)
+        validation_data.append(iris_validation_data)
+    return training_data, validation_data
+
 def main():
     # [ 
     #     [ { folder_name, path, type, bin_value }, { folder_name, path, type, bin_value }, ... ] ,     # each item is an iris's folder
     #     [ { ... }, { ... }, ... ] 
     # ]
     data = load_data() 
+    training, validation = split_data(data)
+
+    print('training', len(training), training)
+    print('validation', len(validation), validation)
 
     ###### TODO: split the list array between training (N - 1) and validation (1) for each class (IRIS)
     print(data[0][0].folder_name)
